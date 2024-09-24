faf7425a0cd17463361addb65a5ced9e6013b430
circutbreaker <jennamboyles@gmail.com>
2024-09-24 10:47:28 -0400

    add files

---
+++ Hello_Digi_IO_CPEBlu.ino
@@ -0,0 +1,34 @@
+///////////////////////////////////////////////////////////////////////////////
+// Circuit Playground Digital In - Hello Digital
+//
+// Author: Carter Nelson
+// MIT License (https://opensource.org/licenses/MIT)
+
+#include <Adafruit_CircuitPlayground.h>
+
+///////////////////////////////////////////////////////////////////////////////
+void setup() {
+  Serial.begin(115200);
+  
+  CircuitPlayground.begin();
+  
+  pinMode(A2, INPUT);
+  pinMode(A3, OUTP ;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void loop() {
+  int reading = digitalRead(A2);
+ 
+  Serial.println(reading);
+  
+  delay(500);
+
+   if (digitalRead(A2) == HIGH) {
+    digitalWrite(A3, HIGH);
+  }
+  else {
+    digitalWrite(A3, LOW);
+  }
+
+}
