eb160310ff0f92cf666309c29633b5bfe3bd6890
circutbreaker <jennamboyles@gmail.com>
2024-09-30 22:20:55 -0400

    add files

---
+++ external_neop_setcolor.ino
@@ -0,0 +1,29 @@
+#include <Adafruit_CircuitPlayground.h>
+// do NOT include the standard NeoPixel library
+
+#define NEOPIX_PIN    A1  //change to whichever pin you have the D1 of your strip connected to
+#define NUM_PIXELS    6   //change if you have more or less
+
+Adafruit_CPlay_NeoPixel strip = Adafruit_CPlay_NeoPixel(NUM_PIXELS, NEOPIX_PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  CircuitPlayground.begin();
+
+  // initialize external NeoPixel strip separately
+  strip.begin();
+}
+
+
+void loop() {
+ CircuitPlayground.clearPixels();
+  // for the external NeoPixels, must use the Adafruit_CPlay_NeoPixel functions directly
+  strip.clear();
+
+  delay(500);
+
+ for (int i = 0; i <= NUM_PIXELS; i++) {
+  strip.setPixelColor(i, 0,   0,   255); //set to blue
+    }
+
+  strip.show();
+}
